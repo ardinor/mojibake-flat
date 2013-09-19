1fad947d0c96ce01ceb36107393a095cc09c13e2
ardinor <temnoyume@gmail.com>
2013-09-19 15:37:34 +1000

    Now we're starting to cook with gas

--- mojibake/app.py
+++ mojibake/app.py
@@ -3,8 +3,22 @@
 from flask import Flask
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
+from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 pages = FlatPages(app)
 freezer = Freezer(app)
+
+assets = Environment(app)
+app.config['ASSETS_DEBUG'] = True
+
+css = Bundle('css/bootstrap.min.css',
+             'css/mojibake.css')
+assets.register('css_all', css)
+
+js = Bundle('js/jquery-2.0.3.min.js',
+            'js/bootstrap.min.js',
+            'js/mojibake.js',
+            filters='rjsmin', output='gen/packed.js')
+assets.register('js_all', js)
